1d0f199acd513ec3702717bdb4a3f2e52c0d76aa
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-04-18 00:08:18 +0200

    Catch errors

--- search_anime/views.py
+++ search_anime/views.py
@@ -1,31 +1,81 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
 
-def get_infos(name, speed):
+def get_source(name):
 	name = name.replace('-', '+')
 	#setup parser
+	print(name)
 	source = requests.get(f'https://www.nautiljon.com/animes/{name}.html').text
 	soup = BeautifulSoup(source, 'lxml')
+	isValid = True
+	if soup.find("meta", attrs={"content": "La page spécifiée n'existe pas"}) is None:
+		return isValid == True, soup
+	else:
+		return isValid == False, soup
 
-	#get informations
-	anime_name = soup.find("h1", class_="h1titre").find("span", attrs={"itemprop": "name"}).text
-	anime_jap = soup.find("ul", class_="mb10").find("span", string='Titre original : ').next_sibling  #regex pour detect les kana
-	date_start = soup.find("ul", class_="mb10").find("span", attrs={"itemprop": "datePublished"}).text
-	date_stop = soup.find("ul", class_="mb10").find("span", attrs={"itemprop": "datePublished"}).next_sibling
-	anime_date = date_start + date_stop
-	anime_site = soup.find("ul", class_="mb10").find("span", string='Simulcast / streaming : ').parent.find_all('a')  #anime_site[0].text
+
+def get_infos(name, speed, soup):
+
+
+	#get usual name
+	try:
+		anime_name = soup.find("h1", class_="h1titre").find("span", attrs={"itemprop": "name"}).text
+	except:
+		anime_name = '🕵️‍♂️ Anime not found, essayer avec un autre nom'
+
+	#get japanese name
+	try:
+		anime_jap = soup.find("ul", class_="mb10").find("span", string='Titre original : ').next_sibling  #regex pour detect les kana
+	except:
+		anime_jap = ''
+
+	#get date
+	try:
+		date_start = soup.find("ul", class_="mb10").find("span", attrs={"itemprop": "datePublished"}).text
+		date_stop = soup.find("ul", class_="mb10").find("span", attrs={"itemprop": "datePublished"}).next_sibling
+		anime_date = date_start + date_stop
+	except:
+		anime_date = 'Inconnu'
+
+
+	anime_web = []
+	try:
+		anime_site = soup.find("ul", class_="mb10").find("span", string='Simulcast / streaming : ').parent.find_all('a')
+		#sort streaming platform
+		for item in anime_site:
+			anime_web.append(item.text)
+	except:
+		anime_site = ['unknown']
+	
+	#get link to the first episode
+	links = []
+	try:
+		anime_links = soup.select(" .nav_vols > div")
+		for link in anime_links:
+			episode = link.find("a", attrs={"rel": "nofollow"}).get('href')
+			links.append(episode)
+	except:
+		pass
+
+
+		
+	#group lists of name and link of website
+	anime_list = zip(anime_web,links)
 
 	#set the title of the page
 	title = anime_name
 	
 	if speed:
-		anime_img = "http://nautiljon.com" + soup.find("a", attrs={"title": "Affiche Originale"}).get('href')
-		return title, anime_name, anime_jap, anime_date, anime_site, anime_img
+		anime_img =''
+		# anime_img = "http://nautiljon.com" + soup.find("a", attrs={"title": "Affiche Originale"}).get('href')
+		return title, anime_name, anime_jap, anime_date, anime_site, anime_list, anime_img
 	else:
-		return title, anime_name, anime_jap, anime_date, anime_site
+		anime_req =''
+		# anime_req = "http://nautiljon.com" + soup.find("a", attrs={"title": "Affiche Originale"}).get('href')
+		return title, anime_name, anime_jap, anime_date, anime_site, anime_list, anime_req
 
 
 
@@ -38,25 +88,43 @@ def get_img(name_anime):
 
 
 
+
 @app.route('/')
 def index():
 	return render_template("index.html", first=True, title='Anime Search')
 
 
+
 @app.route('/v1/view/<name>')
 def see(name):
 	first=False
-	title, anime_name, anime_jap, anime_date, anime_site, anime_img = get_infos(name, True)
+	isValid, soup = get_source(name)
+	if not isValid:
+		return redirect(url_for('index'))
+	title, anime_name, anime_jap, anime_date, anime_site, anime_list, anime_img = get_infos(name, True, soup)
 	return render_template("result.html", **locals())
+
+
 
 @app.route('/v2/view/<name>')
 def view(name):
+	if name == '-' or name == '' or name == '+' or isinstance(name,int):
+		return redirect(url_for('index'))
+	
 	first=False
-	title, anime_name, anime_jap, anime_date, anime_site = get_infos(name, False)
+	isValid, soup = get_source(name)
+	print(isValid)
+	if not isValid:
+		return redirect(url_for('index'))
+
+	title, anime_name, anime_jap, anime_date, anime_site, anime_list, anime_req = get_infos(name, False, soup)
 
 	#get image
-	search_anime = name.replace('-', ' ')
-	anime_img = get_img(search_anime)
+	try:
+		search_anime = name.replace('-', ' ')
+		anime_img = get_img(search_anime)
+	except:
+		anime_img = anime_req
 
 	return render_template("result.html", **locals())
 
