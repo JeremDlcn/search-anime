54c48febd62d360d18e63b9e3cb46422a9bbb71f
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-06-27 20:12:09 +0200

    adding samsite=none

--- search_anime/views.py
+++ search_anime/views.py
@@ -8,7 +8,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 31536000  #production cache
 app.config.update(
     SESSION_COOKIE_SECURE=True,
     SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SAMESITE='Lax',
+    SESSION_COOKIE_SAMESITE='None',
 )
 
 def get_source(name):
